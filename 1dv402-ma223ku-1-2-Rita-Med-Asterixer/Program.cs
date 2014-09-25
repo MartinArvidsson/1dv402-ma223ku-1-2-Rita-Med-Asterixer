b6cd9f8cff24cd2b9a744aeb96c6b427bccdb062
MartinArvidsson <ma223ku@lnu.student.se>
2014-09-25 08:35:41 +0200

    Redovisade, bytte "CaseSwitch" mot %

--- 1dv402-ma223ku-1-2-Rita-Med-Asterixer/Program.cs
+++ 1dv402-ma223ku-1-2-Rita-Med-Asterixer/Program.cs
@@ -10,35 +10,35 @@ namespace Rita_med_asterixer
     {
         static void Main(string[] args)
         {
-            int caseSwitch = 1;
+            //int caseSwitch = 1;
 
             for (int i = 0; i < 25; i++)
             {
                 Console.ResetColor();
 
-                switch (caseSwitch)
+                switch (i % 3) //Byter färg var tredje rad startar som nr. 1
                 {
-                    case 1: Console.ForegroundColor = ConsoleColor.Yellow;
-                        caseSwitch = 2;
+                    case 0: Console.ForegroundColor = ConsoleColor.Yellow; // skriver gul rad, får numret 2
+              //          caseSwitch = 2;
                         break;
 
-                    case 2: Console.ForegroundColor = ConsoleColor.Magenta;
-                        caseSwitch = 3;
+                    case 1: Console.ForegroundColor = ConsoleColor.Magenta; //Skriver rosa rad, får numret 3
+                //        caseSwitch = 3;
                         break;
 
-                    case 3: Console.ForegroundColor = ConsoleColor.Green;
-                        caseSwitch = 1;
+                    case 2: Console.ForegroundColor = ConsoleColor.Green; // Skriver grön rad, för nr 1
+                  //      caseSwitch = 1;
                         break;
 
                 }
-                for (int j = 0; j < 39; j++)
+                for (int j = 0; j < 39; j++) //så länge som j < 0 skriv ut *
                 {
                     Console.Write("* ");
                 }
                 Console.WriteLine();
                 if (i % 2 == 0)
                 {
-                    Console.Write(" ");
+                    Console.Write(" "); //Varannan rad startas med ett mellanslag.
                 }
             }
         }
